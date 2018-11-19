36ec3107ffaf75016d2662149f5efe806dd8186b
Daniel Gabris <DANIEL.GABRIS@emea.zurich.corp>
2018-11-19 17:33:31 +0100

    first exploration and data prep

---
+++ exploratory.R
@@ -0,0 +1,74 @@
+# downloaded datasets from 
+# https://www.kaggle.com/silicon99/dft-accident-data
+# then they were put into full_data folder
+
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(leaflet)
+library(stringr)
+
+join_lookup_func <- function(data_df, lookup_df, field_name) {
+  select_fields <- setdiff(c(colnames(data_df), "label"), field_name)
+  by_statement <- setNames("code", field_name)
+  
+  data_df %>% left_join(lookup_df, by = by_statement) %>%
+    select_(.dots = select_fields) %>% 
+    rename_(.dots = setNames("label", field_name))
+}
+
+police_force_lookup <- read_csv("full_data/lookup/Police_Force.csv")
+accident_severity_lookup <- read_csv("full_data/lookup/Accident_Severity.csv")
+road_type_lookup <- read_csv("full_data/lookup/Road_Type.csv")
+light_conditions_lookup <- read_csv("full_data/lookup/Light_Conditions.csv")
+urban_rural_lookup <- read_csv("full_data/lookup/Urban_Rural.csv")
+police_attended_lookup <- read_csv("full_data/lookup/Police_Officer_Attend.csv")
+
+accidents_full <- read_csv("full_data/Accidents0515.csv")
+casualties_full <- read_csv("full_data/Casualties0515.csv")
+vehicles_full <- read_csv("full_data/Vehicles0515.csv")
+
+accidents_sampled <- accidents_full %>% sample_frac(size = 0.01)
+casualties_sampled <- casualties_full %>% inner_join(accidents_sampled, by = c("Accident_Index"))
+vehicles_sampled <- vehicles_full %>% inner_join(accidents_sampled, by = c("Accident_Index"))
+
+accidents_processed <- accidents_sampled %>% 
+  select(accident_id = Accident_Index,
+         police_department = Police_Force,
+         longitude = Longitude,
+         latitude = Latitude,
+         accident_severity = Accident_Severity,
+         num_vehicles = Number_of_Vehicles,
+         num_casualties = Number_of_Casualties,
+         date = Date,
+         time = Time,
+         road_type = Road_Type,
+         light_conditions = Light_Conditions,
+         weather_conditions = Weather_Conditions,
+         road_conditions = Road_Surface_Conditions,
+         urban_vs_rural = Urban_or_Rural_Area,
+         police_attended = Did_Police_Officer_Attend_Scene_of_Accident
+         ) %>% 
+  join_lookup_func(police_force_lookup, "police_department") %>% 
+  join_lookup_func(accident_severity_lookup, "accident_severity") %>% 
+  join_lookup_func(road_type_lookup, "road_type") %>% 
+  join_lookup_func(light_conditions_lookup, "light_conditions") %>% 
+  join_lookup_func(urban_rural_lookup, "urban_vs_rural") %>% 
+  join_lookup_func(police_attended_lookup, "police_attended") %>% 
+  mutate(police_attended = case_when(
+    str_detect(str_to_lower(.$police_attended), "yes") ~ "Yes",
+    str_detect(str_to_lower(.$police_attended), "no") ~ "No",
+    TRUE ~ NA_character_
+  )) %>% 
+  mutate(light_conditions = case_when(
+    str_detect(str_to_lower(.$light_conditions), "daylight") ~ "Daylight",
+    str_detect(str_to_lower(.$light_conditions), "darkness") ~ "Darkness",
+    TRUE ~ NA_character_
+  )) %>% 
+  mutate(accident_damages = case_when(
+    .$accident_severity == "Slight" ~ runif(n(), min = 1, max = 1000),
+    .$accident_severity == "Serious" ~ runif(n(), min = 1001, max = 10000),
+    .$accident_severity == "Fatal" ~ runif(n(), min = 10001, max = 100000)
+  ) * num_casualties)
+
+write_rds(accidents_processed, "processed_data/accidents_processed.rds", compress = "bz2")
